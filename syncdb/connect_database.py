57e6feb396e473c8c0c2c592821dc5f817dc06a0
Hiroshi <lucas.hh@hotmail.com>
2019-04-13 15:11:34 -0300

    Add files for database connection

---
+++ syncdb/connect_database.py
@@ -0,0 +1,126 @@
+import pymysql
+from config import config
+
+
+class ConnectDatabase:
+    def connect():
+        """ Connect to the database server """
+        conn = None
+        try:
+            # read connection parameters
+            params = config()
+    
+            # connect to the SQL server
+            print('Connecting to the database...')
+            conn = pymysql.connect(**params)
+    
+            # create a cursor
+            cur = conn.cursor()
+            conn.autocommit = True
+
+            # execute a statement
+            print('SQL database version:')
+            cur.execute('SELECT version()')
+    
+            # display the SQL database server version
+            db_version = cur.fetchone()
+            print(db_version)
+        
+            # close the communication with the SQL
+            cur.close()
+        except (Exception, pymysql.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+                print('Database connection closed.')
+
+    def insertAnswer(qid, code, answer, sortorder, assessment_value, language, scale_id):
+        """ Connect to the SQL database server """
+        conn = None
+        try:
+            # read connection parameters
+            params = config()
+            # connect to the SQL server
+            print('Connecting to the SQL database...')
+            conn = pymysql.connect(**params)
+            conn.autocommit = True
+
+            # create a cursor
+            cur = conn.cursor()
+            
+            # execute a statement
+            print('Inserting Answers')
+            cur.execute("""INSERT INTO answers(qid, code, answer, sortorder, assessment_value, language, scale_id) VALUES (%s, %s, %s, %s, %s, %s, %s);""", 
+            (qid, code, answer, sortorder, assessment_value, language, scale_id))
+            # close the communication with the SQL
+            cur.close()
+
+        except (Exception, pymysql.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+                print('Database connection closed.')
+
+    # Not Working
+    # def insertResponse(response):
+    #     """ Connect to the SQL database server """
+    #     conn = None
+    #     try:
+    #         # read connection parameters
+    #         params = config()
+    #         # connect to the SQL server
+    #         print('Connecting to the SQL database...')
+    #         conn = pymysql.connect(**params)
+    #         conn.autocommit = True
+
+    #         # create a cursor
+    #         cur = conn.cursor()
+    #         insert_response
+            
+    #         # execute a statement
+    #         print('Inserting a Response')
+            
+    #         cur.execute(insert_response)
+    #         # close the communication with the SQL
+    #         cur.close()
+
+    #     except (Exception, pymysql.DatabaseError) as error:
+    #         print(error)
+    #     finally:
+    #         if conn is not None:
+    #             conn.close()
+    #             print('Database connection closed.')
+
+    def queryAnswer(qid):
+        """ Connect to the SQL database server """
+        conn = None
+        try:
+            # read connection parameters
+            params = config()
+            # connect to the SQL server
+            print('Connecting to the SQL database...')
+            conn = pymysql.connect(**params)
+            conn.autocommit = True
+
+            # create a cursor
+            cur = conn.cursor()
+            
+            # execute a statement
+
+            cur.execute("""SELECT answer,code FROM limesurvey.answers WHERE qid=%s;""", (qid))
+            query = cur.fetchall()
+            # close the communication with the SQL
+            cur.close()
+
+        except (Exception, pymysql.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+                print('Database connection closed.')
+        return query
+
+if __name__ == '__main__':
+    print((ConnectDatabase.queryAnswer('1')))
